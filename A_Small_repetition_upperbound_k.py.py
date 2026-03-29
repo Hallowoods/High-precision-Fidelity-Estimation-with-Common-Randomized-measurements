90b72c1f4c96513dcb0958ffd6d0f5470c47bbd9
Zhongyi Yang <100124189+Hallowoods@users.noreply.github.com>
2026-03-29 19:45:49 +0800

    Update A_Small_repetition_upperbound_k.py.py

--- A_Small_repetition_upperbound_k.py.py
+++ A_Small_repetition_upperbound_k.py.py
@@ -1,12 +1,12 @@
 """
-upperboundterms_k.py
+The circuit costs NU required for HPFE in THR and CRM shadow estimation based on Clifford measurements. 
 
-This module provides functions for calculating upper bound terms(including cross-characteristic, characteristic, variance, and their amplified version, sample complexity) for snk states with different k. Both for CRM and THR protocol.
+Random Pauli noise is assumed
 
-Pauli noise is assumed.
+The number R of circuit reuse varies
 
 Author: YZY
-Date: 2025-04-01
+
 """
 
 
@@ -25,7 +25,7 @@ from paddle_quantum.qinfo import random_hamiltonian_generator
 from paddle_quantum.trotter import  get_1d_heisenberg_hamiltonian
 from paddle_quantum.intrinsic import _type_fetch, _type_transform
 from paddle_quantum.ansatz import Circuit
-from datetime import datetime  # 导入 datetime 模块
+from datetime import datetime  
 import multiprocessing as mp
 from paddle_quantum.ansatz import Circuit
 from paulichannel_utils import generate_channel
@@ -38,45 +38,39 @@ from itertools import product
 import json
 from multiprocessing import Pool
 
+
+
+
 def generate_pauli_group(nqubit):
-    """生成 nqubit 的 Pauli Group，并将其写入 JSON 和 Pickle 文件"""
+    """Generate n-qubit  Pauli Group，in JSON or Pickle files"""
     pauli_labels = ['x', 'y', 'z']
     pauli_group = []
     json_path = f"pauli_group_{nqubit}qubit.json"
     pkl_path = f"pauli_group_{nqubit}qubit.pkl"
     
     for pauli_ops in product(['I'] + pauli_labels, repeat=nqubit):
-        formatted_pauli = [1]  # 系数固定为 1
+        formatted_pauli = [1]  
         pauli_str = []
-        
-        # 格式化 Pauli 操作，并添加对应 qubit 的位置
         for idx, op in enumerate(pauli_ops):
             if op != 'I':
                 pauli_str.append(f"{op}{idx}")
-        
-        # 如果有非 'I' 操作，则将其加入
         if pauli_str:
-            formatted_pauli.append(','.join(pauli_str))  # Pauli 操作按逗号分隔
-        
-        # 如果没有 Pauli 操作 (只有 'I' 操作)，则跳过
+            formatted_pauli.append(','.join(pauli_str))      
         elif not pauli_str and pauli_ops != tuple(['I'] * nqubit):
             continue
-        
-        # 将每个操作加入 Pauli group
-        pauli_group.append(formatted_pauli)
-
-    # 写入 JSON 文件
+         pauli_group.append(formatted_pauli)
+    # Write into a JSON file
     with open(json_path, "w") as f:
         json.dump(pauli_group, f, indent=4)
 
-    # 写入 Pickle 文件
+    # Write into a Picle file
     with open(pkl_path, "wb") as f:
         pickle.dump(pauli_group, f)
     del pauli_group[0]
-    return pauli_group  # 返回生成的列表
+    return pauli_group 
 
 def load_pauli_group(nqubit):
-    """从 JSON 和 Pickle 文件中读取 Pauli Group"""
+    """Load the Pauli Group from JSON or Picle files"""
     json_path = f"pauli_group_{nqubit}qubit.json"
     pkl_path = f"pauli_group_{nqubit}qubit.pkl"
     
@@ -88,88 +82,56 @@ def load_pauli_group(nqubit):
     
     return pauli_group_json, pauli_group_pickle
 
-def generate_random_state(nqubit,type='random',k=0):
-    if type == 'random':
-        state = pq.state.random_state(nqubit)
-    elif type == 'w_state':
-        state = pq.state.w_state(nqubit)
-    elif type == 'ghz_state':
-        state = pq.state.ghz_state(nqubit)
-    elif type =='bell_state':
-        state = pq.state.bell_state(nqubit)
-    elif type == 'zero_state':
-        state = pq.state.zero_state(nqubit)
-    elif type == 's_nk_state':
-        if k !=0:
-            cir = Circuit(nqubit)
-            cir.h(range(k))
-            cir.t(range(k))
-            state = cir()
-        else:
-            state = pq.state.zero_state(nqubit)
-    state = _type_transform(state, "density_matrix").numpy()
-    np.save('state.npy', state)      
-    return state
-
-
 def save_pauli_channel(channelinfo, filename):
-    '''write-in Pauli Channel file'''
+    """Save the generated Pauli channel from channelinfo, write-in Pauli Channel file"""
     with open(filename, 'wb') as file:
-        pickle.dump(channelinfo, file)  # 将 channelinfo 保存到文件中
+        pickle.dump(channelinfo, file)
 
 def load_pauli_channel(filename):
     '''Read the Pauli Channel file'''
-
     with open(filename, 'rb') as file:
-        channelinfo = pickle.load(file)  # 从文件中读取 channelinfo
+        channelinfo = pickle.load(file)  
     return channelinfo
 
 
 def generate_random_pauli_channel(nqubit,magnitude = 0.1,channel_num = 0):
+    """Generated random Pauli channel, parameterized by magnitude and channel_num. channel_num indicates the number of choices of Pauli noise as an average (default to be 4**n-1), and magnitude controls the amplitude of the noise"""
     channelinfo = []
-    existing_pauli_strings = set()  # 用于跟踪已生成的 Pauli 字符串
+    existing_pauli_strings = set() 
     maxallowed = 4**nqubit - 1
     if channel_num == 0:    #default:random
-        number_of_string = random.randint(1, maxallowed)
+        number_of_string = maxallowed
     else:
         number_of_string = channel_num
     
 
-    while len(channelinfo) < number_of_string:  # 生成指定数量的通道信息
-        coefficient = random.uniform(0, 1)  # 在 [0, 1] 区间生成实数
-        num_pauli = random.randint(1, nqubit)  # 随机生成 Pauli 字符串的数量
-        indices = random.sample(range(nqubit), num_pauli)  # 随机选择不重复的索引
-        pauli_string = ','.join(f'{random.choice("xyz")}{i}' for i in sorted(indices))  # 生成 Pauli 字符串
+    while len(channelinfo) < number_of_string:  
+        coefficient = random.uniform(0, 1)  #  [0, 1] noise probability 
+        num_pauli = random.randint(1, nqubit)  # random chosen Pauli weight
+        indices = random.sample(range(nqubit), num_pauli)  # total number of the randomly chosen sites equals the specific weight
+        pauli_string = ','.join(f'{random.choice("xyz")}{i}' for i in sorted(indices))  # generate the string
         
-        if pauli_string not in existing_pauli_strings:  # 检查字符串是否已存在
-            existing_pauli_strings.add(pauli_string)  # 添加到集合中
-            channelinfo.append([coefficient, pauli_string])  # 添加到 channelinfo 列表
+        if pauli_string not in existing_pauli_strings:  
+            existing_pauli_strings.add(pauli_string)  
+            channelinfo.append([coefficient, pauli_string])  
     sum_p = sum(ele[0] for ele in channelinfo)
     for ele in channelinfo:
         ele[0] = ele[0]/sum_p*magnitude  
-        save_pauli_channel(channelinfo, 'pauli_channel.pkl')  # 保存到文件
+        save_pauli_channel(channelinfo, 'pauli_channel.pkl') 
     return channelinfo,magnitude
-
-
-
 
 
 def caculate_characters_CRM(state,sigma,nqubit):
     '''Calculate cross chatacteristic functions of rho and sigma and charcteristic functions, output as a product of them'''
    
     pauli_group = generate_pauli_group(nqubit=nqubit) #loading Pauli group first
-    # print('pauligroup:',pauli_group)
     delta = state-sigma
     delta_vec = []
     sigma_vec = []
     cross_vec = []
     for i in range(len(pauli_group)):
-        # print(i)
-        # print(pauli_group[i])
         ele = pauli_group[i]
         ele[1] = pq.qinfo.pauli_str_to_matrix([[1,ele[1]]],nqubit).numpy()
-        # pauli =pq.qinfo.pauli_str_to_matrix([[1,pauli_group[i][1]]],nqubit)
-        # print(pauli)
         delta_vec.append(np.trace(delta@ele[1]))  
         sigma_vec.append(np.trace(sigma@ele[1]))
         cross_vec.append(np.trace(delta@ele[1]@sigma@ele[1]))
@@ -189,17 +151,12 @@ def caculate_characters_THR(rho,sigma,nqubit):
     '''Calculate cross chatacteristic functions of rho and sigma and charcteristic functions, output as a product of them'''
    
     pauli_group = generate_pauli_group(nqubit=nqubit) #loading Pauli group first
-    # print('pauligroup:',pauli_group)
     sigma_vec = []
     cross_vec = []
     rho_vec = []
     for i in range(len(pauli_group)):
-        # print(i)
-        # print(pauli_group[i])
         ele = pauli_group[i]
         ele[1] = pq.qinfo.pauli_str_to_matrix([[1,ele[1]]],nqubit).numpy()
-        # pauli =pq.qinfo.pauli_str_to_matrix([[1,pauli_group[i][1]]],nqubit)
-        # print(pauli) 
         sigma_vec.append(np.trace(sigma@ele[1]))
         rho_vec.append(np.trace(rho@ele[1]))
         cross_vec.append(np.trace(rho@ele[1]@sigma@ele[1]))
@@ -216,7 +173,6 @@ def caculate_characters_THR(rho,sigma,nqubit):
 
 
 def compute_for_magnitude(magnow, state, nqubit,N_m):
-    """单个进程的计算任务"""
     d = 2**nqubit
     channel, mag = generate_random_pauli_channel(nqubit, magnitude=magnow,channel_num=3)
     state_noised, l1_norm = generate_channel(state, nqubit, channel)
@@ -238,133 +194,18 @@ def compute_for_magnitude(magnow, state, nqubit,N_m):
     amp_variance= np.real(variance/(1-fidelity)**2)
     sample = int(136*np.log(200)*variance*16/(1-fidelity)**2)
     sample_thr = int(136*np.log(200)*thrifty_variance*16/(1-fidelity)**2)
-    #thrifty ones
     
 
     return [1 / (1 - np.real(fidelity)), sample,np.real(variance),amp_variance,np.real(thrifty_variance),amp_thrifty_variance,sample_thr]
 
-def plot_results(filename,k):
-    """从 CSV 读取数据并绘制曲线"""
-    data = np.loadtxt(filename, delimiter=",", skiprows=1)  # 跳过表头
-    x = data[:, 0]  # 第一列 infid^{-1} 作为横坐标
-    y1 = data[:, 1]  # k_Character
-    y2 = data[:, 2]  # k_Cross_Character
-    y3 = data[:,7]  # k_Character(amp)
-    y4 = data[:,6]  # k_Cross_Character(amp)
-    y5 = data[:,4]  # Real variance
-    y6 = data[:,3]    #sample complexity
-    y7 = data[:,5]    #Real variance(amp)
-    y8 = data[:,8]      #thrifty_variance
-    y9 = data[:,9]      #amp_thrifty_variance]
-    y10 = data[:,10]        ##sample complexity(thrifty)
-    plt.figure(figsize=(8, 6))
-    # plt.scatter(x, y1, marker="o",  label=f"{k}_Character", color="b")
-    # plt.scatter(x, y2, marker="s", label=f"{k}_Cross_Character", color="r")
-    # plt.scatter(x, y3, marker="*",  label=f"{k}_Character(amp)", color="g")
-    # plt.scatter(x, y4, marker="^", label=f"{k}_Cross_Character(amp)", color="orange")
-    # plt.scatter(x, y5, marker="o",  label=f"{k}_Variance", color="b")
-    # plt.scatter(x, y8, marker="x",  label=f"{k}_Variance_thrifty", color="r")
-    plt.scatter(x, y6, marker="x", label=f"{k}_NU", color="r")
-    plt.scatter(x, y10, marker="o", label=f"{k}_NU_THR", color="b")
-    # plt.scatter(x, y7, marker="x",  label=f"{k}_Variance(amp)", color="r")
-    # plt.scatter(x, y9, marker="o",  label=f"{k}_Variance_thrifty(amp)", color="b")
 
 
-    plt.xlabel("infid^{-1}")
-    plt.ylabel("Character Values")
-    plt.title("Character vs infid^{-1}")
-    plt.legend()
-    plt.grid(True)
-    # plt.savefig(filename.replace(".csv", ".png"))  # 保存图片
-    plt.show()
-
-
-# def main(nqubit, k,N_m):
-#     start_time = time.time()
-#     current_time = datetime.now().strftime("%m%d_%H%M")
-#     # # N_m = 1000000
-#     # N_m = 10
-#     # filename = f'A_Small_repetion,shadow_norm,Nm={N_m}_snk_state_n={nqubit},k={k},{current_time}.csv'
-#     # filename = f'shadow_norm_ghz_state_nqubit={nqubit}_Nm={N_m}_{current_time}.csv'
-#     # filename = f'4design_variance_changing:Nm={N_m},for_snk_state_n={nqubit},k={k},{current_time}.csv'
-#     filename = f'clifford_variance_changing:Nm={N_m},for_snk_state_n={nqubit},k={k},{current_time}.csv'
-#     x = np.linspace(1, 50, 20)
-#     magnitudelist = 1 / x  # 1/x spacing
-    
-
-#     # 生成 snk 量子态
-#     if k != 0:
-#         cir = Circuit(nqubit)
-#         cir.h(range(k))
-#         cir.t(range(k))
-#         state = cir()
-#     else:
-#         state = pq.state.zero_state(nqubit)
-#     state = _type_transform(state, "density_matrix").numpy()
-#     # state = pq.state.ghz_state(nqubit)
-#     # state = _type_transform(state, "density_matrix").numpy()
-
-#     # 使用 Pool 进行并行计算
-#     with Pool(processes=10) as pool:  # 10 个进程并行
-#         results = pool.starmap(compute_for_magnitude, [(magnow, state, nqubit,N_m) for magnow in magnitudelist])
-
-#     # 统一写入 CSV 文件
-#     with open(filename, mode='w', newline='') as file:
-#         writer = csv.writer(file)
-#         writer.writerow(['infid^{-1}', f'{k}_Character', f'{k}_Cross_Character', 'Sample#','Variance','Variance(amp)',f"{k}_Character(amp)",f"{k}_Cross_Character(amp)",'Variance_thrifty','Variance_thrifty(amp)','Sample#_thrifty'])
-#         writer.writerows(results)
-
-#     print(f'Data written to {filename}')
-    
-#     # 画图
-#     # plot_results(filename,k)
-
-#     end_time = time.time()
-#     elapsed_time = end_time - start_time
-#     hours, remainder = divmod(int(elapsed_time), 3600)
-#     minutes, seconds = divmod(remainder, 60)
-#     print(f'运行时间: {hours}时{minutes}分{seconds}秒')
-#     return filename
-
-# if __name__ == "__main__":
-#     k = 7
-#     l = np.linspace(1,10,1)
-#     sample_list = [10**int(r) for r in l ]
-#     for Nm in sample_list:
-#         filename = main(nqubit=7, k=k,N_m=Nm)# 画
-#         # plot_results(filename,k)
-#         # plot_results('shadow_norm_snk_state_nqubit=5_k=5_Nm=1000000_0325_1931.csv',k)
-#         variance_amp_values = []
-
-#         # 读取 CSV 文件
-#         with open(filename, mode='r', newline='') as file:
-#             reader = csv.reader(file)
-#             header = next(reader)  # 读取表头
-            
-#             # 找到 'Variance(amp)' 在第几列
-#             index = header.index('Variance(amp)')
-
-#             for row in reader:
-#                 try:
-#                     variance_amp_values.append(float(row[index]))  # 读取并转换为浮点数
-#                 except ValueError:
-#                     pass  # 如果转换失败（例如空值），就跳过
-
-#         # 计算平均值
-#         if variance_amp_values:
-#             mean_variance_amp = np.mean(variance_amp_values)
-#             print(f"Variance(amp) 平均值: {mean_variance_amp}")
-#         else:
-#             print("没有有效的 Variance(amp) 数据")
-
-# if __name__ == "__main__":
-#     main(nqubit=3,k=1)
 def main(nqubit, k, N_m):
     start_time = time.time()
     current_time = datetime.now().strftime("%m%d_%H%M")
 
-    # 修改为你的目标路径
-    folder_path = rf"D:\probability-expectation-project\clifford_variance_changing_Nm\k={k}"
+
+    folder_path = rf"xxxx"         # User defined 
     filename = f'clifford_variance_changing_Nm={N_m}_snk_state_n={nqubit}_k={k}_{current_time}.csv'
     full_path = f"{folder_path}\\{filename}"
 
@@ -383,49 +224,35 @@ def main(nqubit, k, N_m):
     with Pool(processes=10) as pool:
         results = pool.starmap(compute_for_magnitude, [(magnow, state, nqubit, N_m) for magnow in magnitudelist])
 
-    # 写入 CSV 文件到指定目录
+
     with open(full_path, mode='w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['infid^{-1}', 'Sample#','Variance','Variance(amp)','Variance_thrifty','Variance_thrifty(amp)','Sample#_thrifty'])
         writer.writerows(results)
 
     print(f'Data written to {full_path}')
-
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    hours, remainder = divmod(int(elapsed_time), 3600)
-    minutes, seconds = divmod(remainder, 60)
-    print(f'运行时间: {hours}时{minutes}分{seconds}秒')
     return full_path
 
 
 if __name__ == "__main__":
+    #target state generation |S_{n,k}\>
     k = 0
     l = range(1,7,1)
-    sample_list = [10**int(r) for r in l ]
+    sample_list = [10**int(r) for r in l ]        # Number R of circuit reuse
     for Nm in sample_list:
         filename = main(nqubit=7, k=k,N_m=Nm)# 画
-        # plot_results(filename,k)
-        # plot_results('shadow_norm_snk_state_nqubit=5_k=5_Nm=1000000_0325_1931.csv',k)
+
         variance_amp_values = []
 
-        # 读取 CSV 文件
+
         with open(filename, mode='r', newline='') as file:
             reader = csv.reader(file)
-            header = next(reader)  # 读取表头
-            
-            # 找到 'Variance(amp)' 在第几列
+            header = next(reader)  
+
             index = header.index('Variance(amp)')
 
             for row in reader:
                 try:
-                    variance_amp_values.append(float(row[index]))  # 读取并转换为浮点数
+                    variance_amp_values.append(float(row[index]))
                 except ValueError:
-                    pass  # 如果转换失败（例如空值），就跳过
-
-        # 计算平均值
-        if variance_amp_values:
-            mean_variance_amp = np.mean(variance_amp_values)
-            print(f"Variance(amp) 平均值: {mean_variance_amp}")
-        else:
-            print("没有有效的 Variance(amp) 数据")
+                    pass 
